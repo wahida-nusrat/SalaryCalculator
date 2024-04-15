a78d03df97cff544e731cd47807651a66cecacad
Wahida_Nusrat <wahida.nusrat@gmail.com>
2024-04-15 19:19:12 +1000

    Change option for multiple input

--- Project/SalaryCalculator/Program.cs
+++ Project/SalaryCalculator/Program.cs
@@ -3,6 +3,7 @@ using Logging.Interface;
 using SalaryCalculation.Service;
 using SalaryCalculator;
 using SalaryCalculation.IO;
+using System.Runtime.CompilerServices;
 
 // setup configuration from appsettings.cs
 var configuration = Setup.ConfigurationSetup();
@@ -10,15 +11,28 @@ var configuration = Setup.ConfigurationSetup();
 // setup logging framework
 Setup.LoggingSetup();
 Ilogger logger = new Logger();
+mainService();
 
-// take input for gross salary and pay freequency
-var inputServic = new InputServic();
-var userInput = inputServic.UserInput();
 
-// performing all calculations
-var calculationService = new CalculatorService(configuration, logger);
-var taxPayer = calculationService.Calculaton(userInput);
+void mainService()
+{
+    // take input for gross salary and pay freequency
+    var inputServic = new InputServic();
+    var userInput = inputServic.UserInput();
 
-// display income details
-var outputService = new OutputService();
-outputService.Dispaly(taxPayer);
+    // performing all calculations
+    var calculationService = new CalculatorService(configuration, logger);
+    var taxPayer = calculationService.Calculaton(userInput);
+
+    // display income details
+    var outputService = new OutputService();
+    outputService.Dispaly(taxPayer);
+
+    Console.WriteLine("Do you want to continue another calculation? (y/n)");
+    string cont= Console.ReadLine();
+    if (cont.ToLower()=="y")
+    {
+        mainService();
+    }
+    
+}
