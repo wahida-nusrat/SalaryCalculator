413f1836724f79bb4febf1a65d39dd63782c3dd8
wahida-nusrat <167008071+wahida-nusrat@users.noreply.github.com>
2024-04-15 14:06:41 +1000

    Add files via upload

---
+++ Project/SalaryCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Logging.Implementation;
+using Logging.Interface;
+using SalaryCalculation.Service;
+using SalaryCalculator;
+
+// setup configuration from appsettings.cs
+var configuration = Setup.ConfigurationSetup();
+
+// setup logging framework
+Setup.LoggingSetup();
+Ilogger logger = new Logger();
+
+// take input for gross salary and pay freequency
+var inputServic = new InputServic();
+var userInput = inputServic.UserInput();
+
+// performing all calculations
+var calculationService = new CalculatorService(configuration, logger);
+var taxPayer = calculationService.Calculaton(userInput);
+
+// display income details
+var outputService = new OutputService();
+outputService.Dispaly(taxPayer);
---
+++ Project/SalaryCalculator/Setup.cs
@@ -0,0 +1,23 @@
+﻿using SalaryCalculator.Feature.Configuration.Interfaces;
+using SalaryCalculator.Foundation.IoC;
+using Microsoft.Extensions.Configuration;
+using Logging.Interface;
+
+namespace SalaryCalculator
+{
+    public static class Setup
+    {
+        public static IConfiguration ConfigurationSetup()
+        {
+            var serviceProvider = Container.GetServiceProvider<IConfigurationService>();
+            var configuration = serviceProvider.GetAppSettingConfiguration();
+            return configuration;
+        }
+
+        public static void LoggingSetup()
+        {
+            var serviceProvider = Container.GetServiceProvider<Ilogger>();
+            serviceProvider.Configure();
+        }
+    }
+}
